603e690d9b05f6244c20164596512c28660121b0
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-04-30 08:10:38 +0300

    ft:jwt

--- server/config.py
+++ server/config.py
@@ -12,16 +12,16 @@ bcrypt=Bcrypt()
 def create_app():
     app = Flask(__name__)
     CORS(app)
-    app.config['JWT_SECRET_KEY']='36f410a833734793e99e130cf20a25af2b3de1175d02b8450b615c4660d57f02'
-    app.config['JWT_ACCESS_TOKEN_EXPIRES'] = 20000
+    app.config['JWT_SECRET_KEY']='36f410a833734793e99e130cf20a25af2b3de1175d02b8450b615c4660d57f02'#secret key
+    app.config['JWT_ACCESS_TOKEN_EXPIRES'] = 600
     app.config["SQLALCHEMY_DATABASE_URI"]='sqlite:///weather.db'
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    jwt=JWTManager(app)
+    jwt=JWTManager(app) #create a jwt token
 
 
     db.init_app(app)
     bcrypt.init_app(app)
-    jwt.init_app(app)
+    jwt.init_app(app) #attach a jwt token to the app
     migrate.init_app(app,db)
     
     return app
