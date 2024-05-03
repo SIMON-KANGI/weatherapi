969ac212ddc9090a49523fcbf73bf3d21c8db19d
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-05-03 10:52:30 +0300

    home

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Register from './Register'
+import Logout from './Logout'
+
+
+function Home({user,onLogOut}) {
+  return (
+    <div className=''>
+    <h1 className='text-center text-2xl text-blue-700 font-bold'>Welcome to weather broadcast</h1>
+   
+    <br />
+   
+      
+    </div>
+  )
+}
+
+export default Home
