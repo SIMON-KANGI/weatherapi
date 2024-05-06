d80e3752c0892a03d1171d464a8e1fa2339407b0
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-05-06 17:45:44 +0300

    ft:Home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Register from './Register'
 import Logout from './Logout'
-
+import NavBar from '../components/NavBar'
 
 function Home({user,onLogOut}) {
   return (
     <div className=''>
+    
     <h1 className='text-center text-2xl text-blue-700 font-bold'>Welcome to weather broadcast</h1>
    
     <br />
